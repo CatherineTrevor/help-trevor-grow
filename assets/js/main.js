e3f1ef559a825afb1c1434f66bf2fbfd5302df75
Catherine Trevor <catherinetrevor@outlook.com>
2021-03-23 15:01:43 +0000

    add fadeTo and countdown timer functions on results image

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,31 @@ function changeBgColor() {
            document.getElementById('bg_color').style.backgroundColor = "orangered"; 
     }
 }
+
+// Results image to take 15 seconds to fade out on initial image load
+// https://www.w3schools.com/jquery/jquery_fade.asp - help with code. Changed to FadeTo so the page structure doesn't change
+$("#answer").ready(function(){ // on initial results image loading
+  $("#answer").fadeTo(5000, 0.01);  // fade opacity so div box doesn't disappear but image does after 15 seconds
+  timer();
+});  
+
+function displayImage() {
+     $("#answer_image").fadeTo('fast', 1); // change opacity back to 1 so new image displays
+     timer(); // recall countdown timer function on image reload
+     $("#answer_image").fadeTo(5000, 0.01); // fade opacity so div box doesn't disappear but image does after 15 seconds
+};
+
+// Countdown timer over results image to countdown from 15 as the image disappers
+// https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown - code copied and modified
+function timer() {
+    let timeleft = 4;
+    let downloadTimer = setInterval(function(){
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown_timer").innerHTML = "Pick the fruit to feed Trevor!";
+    } else {
+        document.getElementById("countdown_timer").innerHTML = timeleft;
+    } 
+    timeleft -= 1;
+    }, 1000);
+};
