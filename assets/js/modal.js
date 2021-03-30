1a11db3f7161bb4d20d1aa9c1fd0fbf48db7416b
Catherine Trevor <catherinetrevor@outlook.com>
2021-03-30 07:43:00 +0000

    add pop-up modal to change playing level

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,9 +1,10 @@
 // Pop-up modal https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal 
 // Code for modal taken from w3schools.com and CI Handling DOM Events > Loading Events lesson and modified for the site
 
-var welcomeModal = document.getElementById("welcome_modal"); // the welcome modal 
+const welcomeModal = document.getElementById("welcome_modal"); // the welcome modal 
+const changeLevelModal = document.getElementById("select_level_modal"); // the change playing level modal
 
-// Modal open upon page load
+// Welcome Modal open upon page load
 window.onload = (function() {
   welcomeModal.style.display = "block";
 });
@@ -11,20 +12,36 @@ window.onload = (function() {
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == welcomeModal) {
-   /* welcomeModal.style.display = "none";*/
    this.hideModal();
    this.startGame();
   }
 };
 
-// When the user clicks on Ready to play, close the modal
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == changeLevelModal) {
+   this.hideModal();
+   this.startGame();
+  }
+};
+
+// Close the modal
 function hideModal() {
   welcomeModal.style.display = "none";
+  changeLevelModal.style.display = "none";
 }
 
-// Get the <span> element that closes the modal and start game
-var modalCloseBtn = document.getElementsByClassName("close")[0];
-modalCloseBtn.addEventListener('click', startGame); // close the modal
+// When Change level is clicked display welcome modal
+var changeLevelBtn = document.getElementById('change_level_button');
+changeLevelBtn.onclick = function() {
+  changeLevelModal.style.display = "block";
+};
+
+// Close the modal and start game
+var modalCloseBtn = document.querySelectorAll(".close");
+for (let i = 0 ; i < modalCloseBtn.length ; i++){
+    modalCloseBtn[i].addEventListener('click', startGame); // close the modal
+}
 
 // Get the Ready to play text in the welcome modal and start game
 var readyToPlay = document.getElementById("startPlayBtn");
@@ -32,11 +49,11 @@ readyToPlay.addEventListener('click', startGame)
 
 // When How to play is clicked display welcome modal
 var playBtn = document.getElementById('modal_open_link');
-
 playBtn.onclick = function() {
   welcomeModal.style.display = "block";
 };
 
+// Select level of game depending on icon and text at top of playing page
 function selectPlayingLevel(){
     let levelSelected = document.querySelector('.gameSelected').textContent;
     if (levelSelected == "EGG") {
@@ -47,11 +64,10 @@ function selectPlayingLevel(){
         shuffleImagesCaterpillar(); // shuffle and display three option images       
 }};
 
-
 // On modal close or Ready to Play button clicked, start game
 function startGame(){
         hideModal(); // hide modal
-        selectPlayingLevel();  
+        selectPlayingLevel(); // select level of game 
         displayAnswer(); // display answer image
         imageFadeOut(); // start answer image fade out          
         $('.hand').addClass('rotate'); // to start clock from beginning       
