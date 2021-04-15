3f1500c2d4c5decc57ee9b8f3e75c1facb3cdbce
Catherine Trevor <catherinetrevor@outlook.com>
2021-04-15 09:45:26 +0000

    fix CSS, HTML and JS code validation errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+/*jshint esversion: 6 */
+/*globals $:false */
+"-W117";true;
+
 const welcomeModal = document.querySelector('.welcome__modal'); 
 let levelEgg = document.querySelectorAll(".game--level--icon--egg");
 let levelCaterpillar = document.querySelectorAll(".game--level--icon--caterpillar");
@@ -6,13 +10,13 @@ let readyToPlay = document.querySelector('.btn__start__play');
 let closeModal = document.querySelector('.modal__close');
 let colorBtn = document.querySelectorAll('.btn__color__select'); 
 let imageSelect = document.querySelectorAll('.option__image__picture'); 
-const mediaQuery = window.matchMedia('(max-width: 768px)')
+const mediaQuery = window.matchMedia('(max-width: 768px)');
 
 function changeBgColor() {
-    btnSelect = event.target.innerText;
-    bodyColor = document.querySelector('.body');
-    starColor = document.querySelector('.correct__score__star');
-    clockColor = document.querySelector('.clock__face');        
+    let btnSelect = event.target.innerText;
+    let bodyColor = document.querySelector('.body');
+    let starColor = document.querySelector('.correct__score__star');
+    let clockColor = document.querySelector('.clock__face');        
         switch (btnSelect) {
             case "GREEN":
                 bodyColor.style.backgroundColor = "green";
@@ -45,21 +49,22 @@ function changeBgColor() {
                 clockColor.style.backgroundColor = "orangered";
                 break;                                                          
         }  
-};
+}
 function scrollToTop() {
     $('html, body').animate({ scrollTop: 0 }, 'fast');
-};
+}
 function hideModal() {
   welcomeModal.style.display = "none";
   scrollToTop();
-};
+}
 function startCountdownClock() {
     $('.clock__hand').addClass('clock--hands--rotate');
-};
+}
 function resetCountdownClock() {
     $('.clock__hand').removeClass('clock--hands--rotate');
-};
+}
 function shuffleImages() {
+    let imagesArray;
     let currentLevel = document.querySelector('.level__selected').innerHTML;
         switch (currentLevel) {
             case "EGG":
@@ -72,18 +77,17 @@ function shuffleImages() {
                 imagesArray = imagesArrayButterfly;
                 break;                                                
         }
+        let number = Math.floor(Math.random()*imagesArray.length); 
+        let number1 = Math.floor(Math.random()*imagesArray.length);  
+        let number2 = Math.floor(Math.random()*imagesArray.length);
 
-let number = Math.floor(Math.random()*imagesArray.length); 
-let number1 = Math.floor(Math.random()*imagesArray.length);  
-let number2 = Math.floor(Math.random()*imagesArray.length);
+        document.getElementById("imageA").src = imagesArray[number]; 
+        document.getElementById("imageB").src = imagesArray[number1];
+        document.getElementById("imageC").src = imagesArray[number2]; 
 
-document.getElementById("imageA").src = imagesArray[number]; 
-document.getElementById("imageB").src = imagesArray[number1];
-document.getElementById("imageC").src = imagesArray[number2]; 
-
-let answer1 = document.getElementById("imageA").src; 
-let answer2 = document.getElementById("imageB").src;
-let answer3 = document.getElementById("imageC").src;
+        let answer1 = document.getElementById("imageA").src; 
+        let answer2 = document.getElementById("imageB").src;
+        let answer3 = document.getElementById("imageC").src;
 
     switch (answer1) {
         case answer2:
@@ -94,7 +98,7 @@ let answer3 = document.getElementById("imageC").src;
             case answer3:
             shuffleImages();
             break;
-}};
+}}
 let imagesArrayEgg = [
     "./assets/images/egg_level/banana_single1.jpg",
     "./assets/images/egg_level/apple_single1.jpg",
@@ -133,19 +137,19 @@ function displayAnswer() {
 ]; 
     let answerImage = Math.floor(Math.random()*answers.length); 
     document.querySelector('.answer__image__picture').src = answers[answerImage];
-};
+}
 function imageFadeOut() {
     $('.answer__image__picture').fadeTo(5000, 0.01);
-};
+}
 function displayOptionImages() {
     $('.option__image__picture').removeClass('hide');
-};
+}
 function delayDisplayOptionImages() {
   setTimeout(displayOptionImages, 5000);
-};
+}
 function hideOptionImages() {
     $('.option__image__picture').addClass('hide');
-};
+}
 function playGame() {
     shuffleImages();   
     imageFadeIn();        
@@ -153,10 +157,10 @@ function playGame() {
     imageFadeOut();
     delayDisplayOptionImages();
     startCountdownClock();
-};
+}
 function checkAnswer () {
     let answerImage = document.querySelector('.answer__image__picture').src;
-    btnClick = event.target.src;
+    let btnClick = event.target.src;
         switch (answerImage) {
             case btnClick: 
             incrementScore();
@@ -168,14 +172,14 @@ function checkAnswer () {
             default: 
             incorrectAnswerAlert();
         }
-};
+}
 function incrementScore() {
     let oldScore = parseInt(document.querySelector('.correct__score__counter').innerText);
     document.querySelector('.correct__score__counter').innerText = ++oldScore;
-};
+}
 function imageFadeIn() {
     $('.answer__image__picture').fadeTo(100, 1);   
-};
+}
 function correctAnswerAlert () {
     swal({
         title: "You did it!",
@@ -186,7 +190,7 @@ function correctAnswerAlert () {
     .then(() => {
         playGame(); 
     });
-};
+}
 function incorrectAnswerAlert () {
     swal({
         title: "That's not right",
@@ -194,7 +198,7 @@ function incorrectAnswerAlert () {
         icon: "error",
         button: "Try again!",
         });
-};
+}
 function checkScore() {
     let currentLevel = document.querySelector('.level__selected').innerHTML;
     let score = document.querySelector('.correct__score__counter').innerHTML;
@@ -247,22 +251,22 @@ function checkScore() {
                 } 
             break;
         }    
-};
+}
 function playEggLevel() {
     document.querySelector('.level__selected').innerHTML ="EGG";
     $('.game__selected').removeClass('game--level--icon--caterpillar game--level--icon--butterfly').addClass('game--level--icon--egg');
-};
+}
 function playCaterpillarLevel() {
     document.querySelector('.level__selected').innerHTML ="CATERPILLAR";
     $('.game__selected').removeClass('game--level--icon--egg game--level--icon--butterfly').addClass('game--level--icon--caterpillar');
-};
+}
 function playButterflyLevel() {
     document.querySelector('.level__selected').innerHTML ="BUTTERFLY";
     $('.game__selected').removeClass('game--level--icon--egg game--level--icon--caterpillar').addClass('game--level--icon--butterfly');
-};
+}
 function resetScore() {
     document.querySelector('.correct__score__counter').innerHTML = "0";
-};
+}
 function letsPlay() {
     swal({
         title: "Ready?",
@@ -276,8 +280,8 @@ function letsPlay() {
         hideModal();
         hideOptionImages();
         playGame();
-    })
-};
+    });
+}
 function startEggLevel() {
     resetCountdownClock();
     hideOptionImages();
@@ -285,7 +289,7 @@ function startEggLevel() {
     imageFadeIn();
     letsPlay();
     playEggLevel();
-};
+}
 function startCaterpillarLevel() {
     resetCountdownClock();
     hideOptionImages();
@@ -293,7 +297,7 @@ function startCaterpillarLevel() {
     imageFadeIn();
     letsPlay();
     playCaterpillarLevel();
-};
+}
 function startButterflyLevel() {
     resetCountdownClock();
     hideOptionImages();
@@ -301,26 +305,26 @@ function startButterflyLevel() {
     imageFadeIn();
     letsPlay();
     playButterflyLevel();
-};
+}
 
 readyToPlay.addEventListener('click', hideModal);
 readyToPlay.addEventListener('click', playGame);
 closeModal.addEventListener('click', letsPlay);
 for (let i = 0; i < levelEgg.length ; i++){
 levelEgg[i].addEventListener('click', startEggLevel);
-};
+}
 for (let i = 0; i < levelCaterpillar.length ; i++){
 levelCaterpillar[i].addEventListener('click', startCaterpillarLevel);
-};
+}
 for (let i = 0; i < levelButterfly.length ; i++){
 levelButterfly[i].addEventListener('click', startButterflyLevel);
-};
+}
 for (let i = 0 ; i < colorBtn.length ; i++){
   colorBtn[i].addEventListener('click', changeBgColor);
-};
+}
 for (let i = 0 ; i < imageSelect.length ; i++){
   imageSelect[i].addEventListener('click', checkAnswer);
-};
+}
 if (mediaQuery.matches) {
   $('.footer').removeClass('fixed-bottom');
-};
+}
